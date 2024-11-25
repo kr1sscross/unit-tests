ea75f7511a65eae66113f005cd23d90abb612809
kr1sscross <krystian.krawczyk@int.pl>
2024-11-26 00:04:25 +0100

    Update test_mark_attendance.py

--- test_mark_attendance.py
+++ test_mark_attendance.py
@@ -5,7 +5,7 @@ class TestMarkAttendance(unittest.TestCase):
 
     @patch('builtins.input', side_effect=["y", "n"])
     def test_mark_attendance(self, mock_input):
-        from import_eksport_files.py import mark_attendance
+        from import_eksport_files import mark_attendance
         students = [{'name': 'John', 'present': False}, {'name': 'Jane', 'present': False}]
         mark_attendance(students)
         self.assertTrue(students[0]['present'])
