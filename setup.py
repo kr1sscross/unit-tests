ace4024f73f481dc7d6efc8fe0af1ee3782baa83
kr1sscross <krystian.krawczyk@int.pl>
2024-12-23 18:17:14 +0000

    version changed

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-# Read the contents of your README file
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
+Version = os.getenv('PACKAGE_VERSION')
+
 setup(
     name='unit_tests_kr1sscross',
-    version='0.1.0',
+    version=Version,
     description='A sample Python function package',
     long_description=long_description,
     long_description_content_type='text/markdown',
